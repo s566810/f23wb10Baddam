05882eeb5f16dbe7b0804ff4897af8ba3439a037
s566810 <143108262+s566810@users.noreply.github.com>
2023-10-31 22:13:41 -0500

    added routes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var schoolRouter = require('./routes/school');
+var boardRouter = require('./routes/board');
+
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/schools', schoolRouter);
+app.use('/board', boardRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
