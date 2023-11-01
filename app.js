34cbc602e2743a04f702ea2933d75fb5b8716794
s566810 <143108262+s566810@users.noreply.github.com>
2023-11-01 16:07:49 -0500

    added choose

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var schoolRouter = require('./routes/school');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 
 var app = express();
@@ -26,7 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/schools', schoolRouter);
 app.use('/board', boardRouter);
-
+app.use('/choose', chooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
